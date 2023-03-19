0aca4d22b4f53d340cf6025519bb400ec848a613
Leo0114 <leonel0614@gmail.com>
2023-03-19 15:32:06 -0600

    as

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Leo0114.github.io",
-  base: "/astro-guitarls",
+  /*   site: "https://Leo0114.github.io",
+  base: "/astro-guitarls", */
   integrations: [react()],
 });
